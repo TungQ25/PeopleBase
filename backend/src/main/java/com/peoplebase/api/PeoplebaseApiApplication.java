fd7bbd2ca7a04d552714ce69e15c77b014422778
TungQ25 <qtung.work@gmail.com>
2026-08-20 14:17:58 +0700

    Initial commit

---
+++ backend/src/main/java/com/peoplebase/api/PeoplebaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.peoplebase.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplebaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeoplebaseApiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/peoplebase/api/PeoplebaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peoplebase.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplebaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
